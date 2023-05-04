36cda6054994001d4825f90e7c1bd8d5f1b991f3
Sanohehe <edwardhayashi7266@gmail.com>
2023-05-04 02:05:46 -0400

    Fixed, senate and house are actually different data now.

--- ProPublica_data.py
+++ ProPublica_data.py
@@ -1,15 +1,17 @@
 from main import *
 # Set up the API endpoint and headers
-api_endpoint = "https://api.propublica.org/congress/v1/116/senate/members.json"
+api_endpoint_senate = "https://api.propublica.org/congress/v1/117/senate/members.json"
+api_endpoint_house = "https://api.propublica.org/congress/v1/117/house/members.json"
 api_headers = {"X-API-Key": PROPUBLICA}
 
 # Make the API request
-response = requests.get(api_endpoint, headers=api_headers)
+response_senate = requests.get(api_endpoint_senate, headers=api_headers)
+response_house = requests.get(api_endpoint_house, headers=api_headers)
 
 # Check if the request was successful
-if response.status_code == 200:
+if response_senate.status_code == 200:
     # Retrieve the data from the API response
-    data = response.json()
+    data = response_senate.json()
 
     # Extract the data for each member of Congress
     members = data["results"][0]["members"]
@@ -19,20 +21,19 @@ if response.status_code == 200:
         json.dump(members, outfile, indent=4)
 
     # Print a message indicating that the data was
-    print("Member data saved to member_data.json")
+    print("This is from session 2021/1/3 - 2023/1/3")
+    print("Member data saved to member_senate_data.json")
+
+    # Print the number of members in the Senate
+    num_members = len(members)
+    print(f"Number of members in the Senate: {num_members}")
+
 else:
-    # Handle errors, such as logging the error or raising an exception
-    print(f"Error: {response.status_code}")
+    print(f"Error: {response_senate.status_code}")
 
-api_endpoint = "https://api.propublica.org/congress/v1/116/house/members.json"
-
-# Make the API request
-response = requests.get(api_endpoint, headers=api_headers)
-
-# Check if the request was successful
-if response.status_code == 200:
+if response_house.status_code == 200:
     # Retrieve the data from the API response
-    data = response.json()
+    data = response_house.json()
 
     # Extract the data for each member of Congress
     members = data["results"][0]["members"]
@@ -42,7 +43,11 @@ if response.status_code == 200:
         json.dump(members, outfile, indent=4)
 
     # Print a message indicating that the data was
-    print("Member data saved to member_data.json")
+    print("Member data saved to member_house_data.json")
+
+    # Print the number of members in the Senate
+    num_members = len(members)
+    print(f"Number of members in the House: {num_members}")
 else:
     # Handle errors, such as logging the error or raising an exception
-    print(f"Error: {response.status_code}")
+    print(f"Error: {response_house.status_code}")
